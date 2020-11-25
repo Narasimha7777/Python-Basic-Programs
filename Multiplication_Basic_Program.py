35e634d0e32f545eb056a25928533768f6dc9d47
Narasimha7777 <narasimhareddy702@gmail.com>
2020-11-25 13:47:53 +0000

    Changed variable names

--- Multiplication_Basic_Program.py
+++ Multiplication_Basic_Program.py
@@ -1,7 +1,8 @@
 # This is a sample Python script.
 
-x = input("Enter Hours: ")
-y = input("Enter Rate: ")
-a = int(x)
-b = int(y)
-print(a * b)
+num1_input = input("Enter Hours: ")
+num2_input = input("Enter Rate: ")
+num1 = int(num1_input)
+num2 = int(num2_input)
+multiplication = num1 * num2
+print(multiplication)
